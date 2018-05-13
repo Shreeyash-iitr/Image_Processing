348bba3371eb452e793e04b4abe9ae60ddb0e0c2
Shreeyash-iitr <sgeda@ee.iitr.ac.in>
2018-05-13 11:37:21 +0530

    mask operation updated

--- filter2D_mask_operation.py
+++ filter2D_mask_operation.py
@@ -9,36 +9,58 @@ import cv2
 import numpy
 # importing numpy
 
+from PIL import Image
+#importing PIL.Image
+
 img = cv2.imread('src.JPG')
 # reading image from computer
 
-res = numpy.zeros((img.shape[0], img.shape[1], 3),numpy.uint64)
+res = numpy.zeros((img.shape[0], img.shape[1], 3),numpy.uint8)
 # making a numpy image object of same size as img
 
 
-temp = numpy.zeros((img.shape[0]+2, img.shape[1]*3+6), numpy.uint8)
+temp = numpy.zeros((img.shape[0]+2, img.shape[1]+2,3), numpy.uint8)
 for row in range(0, img.shape[0]):
-    for col in range(0, img.shape[1]*3):
-        temp[row + 1][col + 3] = img[row][col]
+    for col in range(0, img.shape[1]):
+        for color in range(0,3):
+            temp[row + 1][col + 1][color] = img[row][col][color]
 
       #  for color in range(0, 3):
-
 # making an image temp same as img having two extra(at boundaries) row and column initialized with zero
+
+
 
 for i in range(1, temp.shape[0]-1):
     for j in range(1, temp.shape[1]-1):
         for k in range(0,3):
-            #res[i-1][j-1][k] = \
-            print(temp[i][j][k] - temp[i-1][j][k] - temp[i+1][j][k] - temp[i][j-1][k] - temp[i][j+1][k])
-# multiplying mask matrix to get new image
+            res[i-1][j-1][k] = ((5*temp[i][j][k]) - (temp[i-1][j][k] + temp[i+1][j][k] + temp[i][j-1][k] + temp[i][j+1][k]))
+            print(res[i-1][j-1][k])
+            # multiplying mask matrix to get new image
 #      mask =     [0  -1   0
 #                 -1   5  -1
 #                  0  -1   0]
 
-#cv2.imshow('original', img)
-#cv2.imshow('temp',temp)
-#cv2.imshow('masked.JPG', res)
-#cv2.waitKey()
+
+
+kernf = numpy.zeros([3,3])
+kernf[0,1] = -1
+kernf[1,2] = -1
+kernf[1,0] = -1
+kernf[2,1] = -1
+kernf[1,1] = 5
+print(kernf)
+func = cv2.filter2D(img,-1,kernf)
+# to match our output image with cv2.filter2D function's output image
+
+
+
+cv2.imshow('original', img)
+cv2.imshow('kernf',func)
+cv2.imshow('masked.JPG', res)
+cv2.waitKey()
+
+
+# result is almost same as functions output except at some pixels. The mask I used here is used for smoothening.
 
 
 
