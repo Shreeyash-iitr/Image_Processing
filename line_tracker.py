3c51176045383094cd1ed73af4290d2d028aa1a6
Shreeyash-iitr <sgeda@ee.iitr.ac.in>
2018-05-16 18:55:29 +0530

    updating tracker code

--- line_tracker.py
+++ line_tracker.py
@@ -11,25 +11,28 @@ while True:
     # track = cv2.imread('track2.jpg')
     #cv2.namedWindow('thresh', cv2.WINDOW_NORMAL)
     gray = cv2.cvtColor(track,cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
-    dst = [96, 192, 288, 384]
+    ret, thresh1 = cv2.threshold(gray, 70, 255, cv2.THRESH_BINARY)
+    thresh = cv2.resize(thresh1,(200,400))
+    dst = [80, 160, 240, 320]
 
     for row in dst:
         count = 0
-        for col in range(0, 640):
+        for col in range(0, 200):
             if all([thresh[row][col] == 0, count == 0]):
                 x1 = col
                 count = 1
 
             elif all([thresh[row][col] == 255, count == 1]):
                 x2 = col - 1
+                k=9
+
 
                 break
-        if (count == 1):
+        if all([count == 1,k==9]):
             cv2.circle(thresh, ((x1 + x2) // 2, row), 5, (255, 0, 0), -1)
 
     for row in dst:
-        cv2.circle(thresh, (320, row), 5, (0, 255, 0), -1)
+        cv2.circle(thresh, (200, row), 5, (0, 255, 0), -1)
 
     cv2.imshow('thresh', thresh)
 
