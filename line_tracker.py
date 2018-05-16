d36f44318997e5160602a4e25c266ea9820b3997
Shreeyash-iitr <sgeda@ee.iitr.ac.in>
2018-05-16 17:56:02 +0530

    adding line follower code

--- line_tracker.py
+++ line_tracker.py
@@ -1,35 +1,38 @@
 import cv2
-t1 = cv2.getTickCount()
-x1=0
-x2=0
-track = cv2.imread('track3.jpg')
-cv2.namedWindow('thresh', cv2.WINDOW_NORMAL)
-ret, thresh = cv2.threshold(track, 50, 255, cv2.THRESH_BINARY)
-dst = [653,1306,1959,2612]
-pix = 0
-for row in dst:
-    count = 0
-    for col in range(0, 1836):
-        if all([thresh[row][col][0] == 0, count == 0]):
-            x1 = col
-            count = 1
-            #col=col+10
+import numpy
+import urllib
 
-        elif all([thresh[row][col][0] == 255, count == 1]):
-            x2 = col - 1
+url='http://192.168.0.105:8080/shot.jpg'
+while True:
+    imgresp = urllib.request.urlopen(url)
+    imgNP = numpy.array(bytearray(imgresp.read()),dtype=numpy.uint8)
+    track = cv2.imdecode(imgNP,-1)
 
-            break
-    if (count==1):
-        cv2.circle(thresh, ((x1 + x2) // 2, row), 50, (255, 0, 0), -1)
+    # track = cv2.imread('track2.jpg')
+    #cv2.namedWindow('thresh', cv2.WINDOW_NORMAL)
+    gray = cv2.cvtColor(track,cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+    dst = [96, 192, 288, 384]
 
+    for row in dst:
+        count = 0
+        for col in range(0, 640):
+            if all([thresh[row][col] == 0, count == 0]):
+                x1 = col
+                count = 1
 
+            elif all([thresh[row][col] == 255, count == 1]):
+                x2 = col - 1
 
-for row in dst:
-    cv2.circle(thresh, (918, row), 50, (0, 255, 0), -1)
+                break
+        if (count == 1):
+            cv2.circle(thresh, ((x1 + x2) // 2, row), 5, (255, 0, 0), -1)
 
+    for row in dst:
+        cv2.circle(thresh, (320, row), 5, (0, 255, 0), -1)
 
-cv2.imshow('thresh', thresh)
-t2= cv2.getTickCount()
-print((t2-t1)/cv2.getTickFrequency())
+    cv2.imshow('thresh', thresh)
 
-cv2.waitKey()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
